c2efb3bd64b2772792db56d9f3d74f20287f1ccd
Spencer Blum <spencerblum@gmail.com>
2020-01-30 20:30:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ def list_of_directors(source)
 new_array = []
 index = 0
   while i < source.length do
-    new_array << source[i][:name]
+    new_array.push(source[i][:name])
     index += 1
   end
 new_array
